b5a69f16cbb51ce301a8a0bfab138fc42a5e4966
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2021-05-12 14:35:36 -0300

    Projeto criado a partir do SpringInitializer

---
+++ src/main/java/br/miranda/zup/proposta/desafioDeProposta/DesafioDePropostaApplication.java
@@ -0,0 +1,13 @@
+package br.miranda.zup.proposta.desafioDeProposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDePropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/miranda/zup/proposta/desafioDeProposta/DesafioDePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.miranda.zup.proposta.desafioDeProposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDePropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
